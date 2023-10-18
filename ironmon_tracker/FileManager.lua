cf537c84ccf6d55a8f6364612c237f05cdd8d87f
seadogstingray <seadogstingray@gmail.com>
2023-10-18 15:15:44 -0400

    fixing updater endpoint

--- ironmon_tracker/FileManager.lua
+++ ironmon_tracker/FileManager.lua
@@ -59,8 +59,8 @@ FileManager.Extensions = {
 
 FileManager.Urls = {
 	VERSION = "https://api.github.com/repos/seadogstingray/Ironmon-gen-2-tracker/releases/latest",
-	DOWNLOAD = "https://api.github.com/repos/seadogstingray/Ironmon-gen-2-tracker/releases/latest",
-	WIKI = "https://api.github.com/repos/seadogstingray/Ironmon-gen-2-tracker/releases/latest",
+	DOWNLOAD = "https://github.com/repos/seadogstingray/Ironmon-gen-2-tracker/releases/latest",
+	WIKI = "https://github.com/repos/seadogstingray/Ironmon-gen-2-tracker/releases/latest",
 	EXTENSIONS = "https://github.com/besteon/Ironmon-Tracker/wiki/Tracker-Add-ons#custom-code-extensions",
 }
 
